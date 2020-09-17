4bbdef1c52138b9079060a7375efeddec606894d
carterhalvorson <826628@mystma.org>
2020-09-17 20:10:50 +0000

    top5 movies

---
+++ unit1/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("#1 UP");
+        System.out.println("#2 whats up arnold");
+        System.out.println("#3 The vegie tales movie");
+        System.out.println("#4 Garfield a tale of two kitties");
+        System.out.println("#5 It");
+    }
+}
