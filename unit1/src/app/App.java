08f85c090b869fd0bb560cc7d7ad2eebdeb2d5bc
carterhalvorson <826628@mystma.org>
2020-09-25 20:07:24 +0000

    counting

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,23 +1,32 @@
 package app;
-import java.util.Scanner;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
      
-        Scanner input = new Scanner(System.in);
+        int count = 0;
+        String color = "red";
+        boolean amIInSchool = true;
 
-        System.out.println("please enter first name");
-        String firstName = input.nextLine();
-        System.out.println("please enter last name");
-        String lastName = input.nextLine();
-        System.out.println("please enter your age");
-        String age = input.nextLine();
-        System.out.println("please enter Grade");
-        String grade = input.nextLine();
-        System.out.println("please enter favorite color");
-        String color = input.nextLine();
+        while (count <= 10) {
+            System.out.println("the count is " + count);
+            count = count + 1;
+        }
+        System.out.println("count finished");
 
-        System.out.println("your name is " + firstName + lastName + " your age is " + age + " your grade is " + grade + " your favorite color is " + color);
+        while (color == "red"){
+            System.out.println("the color is " + color);
+            color = "green";
+        }
+        System.out.println("color finished");
 
+        while (amIInSchool == true)  {
+            System.out.println("are you in school? " + amIInSchool);
+            amIInSchool = false;
+        }
+            System.out.println("you are in school");
 
+        
     }
 }
