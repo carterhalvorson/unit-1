efdc0873478a19f0f42ca79a4d53d81deaed7224
carterhalvorson <826628@mystma.org>
2020-10-08 20:00:33 +0000

    Work in proggress

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,31 +5,20 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-for (int i = 10; i <= 20; i = i + 1){
-    System.out.println("you are at " + i);
-    System.out.println("keep counting ");
-}
-System.out.println(" your journey begins 1/5 ");
-for (int i = 10; i <= 100; i = i + 10){
-    System.out.println("you are at " + i );
-    System.out.println("keep going ");
-            }
-    System.out.println("2/5");
-    for (int i = 1000; i <= 256000; i = i + 1000){
-    System.out.println("you are at " + i);
-    System.out.println("your getting closer ");
-}
-System.out.println("3/5");
-for (int i = 11; i <= 12; i = i + 1){
-    System.out.println("you are at " + i );
-    System.out.println("this is easy ");
-        
-    }
-    System.out.println("4/5");
-    for (int i = 10; i <= 0; i = i - 1){
-    System.out.println("you are at " + i );
-    System.out.println("SO CLOSE ");
+for (int i = 0; i <= 1000; i = i +1){
+    System.out.println("The count is " + i);
     
-    }
-    System.out.println(" finaly done! ");
-}}
+
+}
+
+String Str = "abc";
+
+if ( );{
+System.out.println("Correct");
+System.out.println(" Your prize is the friends you made along the way ");
+} 
+else { System.out.println("Incorrect");
+System.out.println( "Try again");
+}
+
+}
