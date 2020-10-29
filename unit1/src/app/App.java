2dce70c799194b348afa770a1e7107cf917099fc
carterhalvorson <826628@mystma.org>
2020-10-29 19:52:55 +0000

    password

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,28 +1,27 @@
 package app;
 
-
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-for (int i = 0; i <= 1000; i = i +1 ){
-    System.out.println("The count is " + i);
-    
-System.out.println("Please Enter Password");
+        Scanner input = new Scanner(System.in);
+           
+        int tries = 0;  
+     
+while (tries < 3 ){
+     System.out.println("Please Enter Password");
+            String enteredPassword = input.nextLine();
+        if (enteredPassword.equals("john")) {
+            System.out.println("Correct");
+            System.out.println(" Your prize is the friends you made along the way ");
+        } else {
+            System.out.println("Incorrect");
+            System.out.println("Try again");
+            tries++;
+
+        }
+    }
+
+    }
 }
-
-String Str = "abc";
-
-if (Str == "abc");{
-System.out.println("Correct");
-System.out.println(" Your prize is the friends you made along the way ");
-}else{ System.out.println("Incorrect");
-System.out.println( "Try again");
-
-
-
-
-
-}
-
-}}
