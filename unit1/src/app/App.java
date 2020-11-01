6ac121a42af1e465d4223f4fb7811dd4cf53a309
carterhalvorson <826628@mystma.org>
2020-11-01 01:59:20 +0000

    Ch 3 done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,23 +5,28 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner input = new Scanner(System.in);
-           
-        int tries = 0;  
-     
-while (tries < 3 ){
-     System.out.println("Please Enter Password");
-            String enteredPassword = input.nextLine();
-        if (enteredPassword.equals("john")) {
-            System.out.println("Correct");
-            System.out.println(" Your prize is the friends you made along the way ");
-        } else {
-            System.out.println("Incorrect");
-            System.out.println("Try again");
-            tries++;
+    Scanner input = new Scanner(System.in);
 
-        }
+    int targetNum = 33;
+    boolean isComplete = false;
+
+    while (isComplete == false) {
+         System.out.println("guess a number between 0 and 100");
+         int guess = input.nextInt();
+         input.nextLine();
+    if (guess == targetNum) {
+        System.out.println("Correct");
+        isComplete = true;
     }
+    else if (guess < targetNum) {
+        System.out.println("Low");
+    }
+    else {
+        System.out.println("High");
+    }
+    }
+        }
+    
 
 
 String line[] = {"Ditty", "Daisy", "Yoshi", "Toad", "Wario", "DK", "Luigi", "Peach", "Mario", "Waluigi"};
