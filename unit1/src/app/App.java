07a1f83c291efdee5636e5cb8466e269adef3ef1
carterhalvorson <826628@mystma.org>
2020-11-12 20:48:18 +0000

    challenge 4

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,41 +4,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-     helloWorld();
-     firstName();
-     middleName();
-     lastName();
-     isCool("Carter");
-     number(11);
-multiply(4, 6, 2, 8);
-    }
+     Scanner input = new Scanner(System.in);
 
-    public static void helloWorld(){
-        System.out.println("Hello world");
-    }
-    public static void firstName(){
-         System.out.println("Carter");
-    }
-    public static void middleName(){
-        System.out.println("Bennet");
-    }
-    public static void lastName(){
-        System.out.println("Halvorson");
-    }
-    public static void isCool(String name){
-System.out.println(name + " is cool");
+     System.out.println("Please enter a number");
+     int num1 = input.nextInt();
+     input.nextLine();
+     
+     System.out.println("Please enter another number");
+     int num2 = input.nextInt();
+     input.nextLine();
 
-    }
-    public static void number(int number){
-    if ( number > 10){
-System.out.println(number + " is greater than 10");
-    }else {
-        System.out.println(number + " is less than 10");
-
-    }
-    }
-    public static void multiply(int num1,int num2,int num3,int num4){
-        System.out.println(num1 * num2 * num3 * num4 );
+     for (int i = num1; i < num2; i++){
+         if ( i % 2 == 0){
+             System.out.println(i);
+             
+         }
+     }
 
     }
     
